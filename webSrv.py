982a59a7920e02c751b336fd16cfa8c48dff5abf
Christina Rost <christina.rost@hfg-gmuend.de>
2018-07-27 02:01:01 +0200

    changed colors on website for better visibility

--- webSrv.py
+++ webSrv.py
@@ -177,108 +177,108 @@ def _httpHandlerAlarm(httpClient, httpResponse):
     html ="""\
         <html lang=de>
 
-<head>
-  <title>RainbowWarrior Alarm Settings</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="bootstrap.min.css">
-  <link rel="stylesheet" href="style.css">
-</head>
+        <head>
+          <title>RainbowWarrior Uhr- und Weckzeiteinstellungen</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <link rel="stylesheet" href="bootstrap.min.css">
+          <link rel="stylesheet" href="style.css">
+        </head>
 
-<body class="h-100">
-  <div class="ground"></div>
-  <div class="sky">
-    <div class="cloud variant-1"></div>
-    <div class="cloud variant-2"></div>
-    <div class="cloud variant-3"></div>
-    <div class="cloud variant-4"></div>
-    <div class="cloud variant-5"></div>
-  </div>
-  <div class="rainbow-preloader">
-    <div class="rainbow-stripe"></div>
-    <div class="rainbow-stripe"></div>
-    <div class="rainbow-stripe"></div>
-    <div class="rainbow-stripe"></div>
-    <div class="rainbow-stripe"></div>
-    <div class="shadow"></div>
-    <div class="shadow"></div>
-  </div>
-  <div class="container text-white align-items-center d-flex flex-column h-100 justify-content-end pb-md-5">
-    <div class="row w-100">
-      <div class="col-md-6">
-        <h1>Time and alarm config</h1>
-        <p>
-          {0:02.02d}.{1:02.02d}.{2:02.02d} {3:02.02d}:{4:02.02d} Uhr
-        </p>
-        <form method="POST" action="/alarm" id="setTime">
-          <h2 form="setTime">Systemzeit einstellen</h2>
-          <div class="row">
-            <div class="col-md-6">
-              <label for "year">Datum</label>
-              <input class="form-control" type="text" name="date" id="date" value="{0:02.02d}.{1:02.02d}.{2:02.02d}">
-            </div>
-            <div class="col-md-6">
-              <label for "time">Uhrzeit</label>
-              <input class="form-control" type="text" name="time" id="time" value="{3:02.02d}:{4:02.02d}">
-            </div>
+        <body class="h-100">
+          <div class="ground"></div>
+          <div class="sky">
+            <div class="cloud variant-1"></div>
+            <div class="cloud variant-2"></div>
+            <div class="cloud variant-3"></div>
+            <div class="cloud variant-4"></div>
+            <div class="cloud variant-5"></div>
           </div>
-          <button class="btn btn-primary" type="reset">Eingabe zur&uuml;cksetzen</button>
-          <button class="btn btn-primary" type="submit">absenden</button>
-        </form>
-        <form method="POST" action="/alarm" id="setAlarm">
-          <h2 form="setAlarm">Weckzeit einstellen</h2>
-          <div class="row">
-            <div class="col-md-6">
-              <label for "alarmTime">Uhrzeit</label>
-              <input class="form-control" type="text" name="alarmTime" id="alarmTime" value="{5:02.02d}:{6:02.02d}">
-            </div>
-            <div class="col-md-6">
-              <label for "alarmTime">Uhrzeit</label>
-              <input type="checkbox" name="dailyAlarm" value="dailyAlarm" checked="checked"> daily Alarm
-              </label>
-            </div>
+          <div class="rainbow-preloader">
+            <div class="rainbow-stripe"></div>
+            <div class="rainbow-stripe"></div>
+            <div class="rainbow-stripe"></div>
+            <div class="rainbow-stripe"></div>
+            <div class="rainbow-stripe"></div>
+            <div class="shadow"></div>
+            <div class="shadow"></div>
           </div>
-          <button class="btn btn-primary" type="submit" name="setAlarm" value="setAlarm">Eingabe absenden</button>
-        </form>
-      </div>
-      <div class="col-md-6">
-        <form method="POST" action="/alarm" id="setSound">
-          <h2 form="setSound">Wecksound w&auml;hlen</h2>
-          </br>
-          <input type="radio" id="MarioMain" name="setSound" value="MarioMain">
-          <label for="MarioMain"> Super Mario - Main Theme</label>
-          </br>
-          <input type="radio" id="MarioTitle" name="setSound" value="MarioTitle">
-          <label for="MarioTitle"> Super Mario - Title Music</label>
-          </br>
-          <input type="radio" id="Looney" name="setSound" value="Looney">
-          <label for="Looney"> Looney Tunes Theme</label>
-          </br>
-          <input type="radio" id="Simpsons" name="setSound" value="Simpsons">
-          <label for="Simpsons"> The Simpsons</label>
-          </br>
-          <input type="radio" id="Indiana" name="setSound" value="Indiana">
-          <label for="Indiana"> Indiana Jones</label>
-          </br>
-          <input type="radio" id="Muppets" name="setSound" value="Muppets">
-          <label for="Muppets"> The Muppet Show</label>
-          </br>
-          <input type="radio" id="Gadget" name="setSound" value="Gadget">
-          <label for="Gadget"> Inspector Gadget</label>
-          </br>
-          <input type="radio" id="StarWars" name="setSound" value="StarWars">
-          <label for="StarWars"> Star Wars Theme</label>
-          </br>
-          <input type="radio" id="Tetris" name="setSound" value="Tetris">
-          <label for="Tetris"> Tetris Theme</label>
-          </br>
-          <button class="btn btn-primary" type="submit" name="setSongButton" value="setSong">Song abspielen</button>
-          <button class="btn btn-primary" type="submit" name="playSongButton" value="playSong">Wecksound w&auml;hlen</button>
-        </form>
-      </div>
-    </div>
-</body>
-
-</html>""".format(date[2], date[1], date[0], date[3], date[4], alarmTime[0], alarmTime[1])#.format(0, 1, 2, 3,4, 5)#
+          <div class="container text-white align-items-center d-flex flex-column h-100 justify-content-end pb-md-5">
+            <div class="row w-100">
+              <div class="col-md-6">
+                <h1>Uhrzeit und Weckzeit einstellen</h1>
+                <p>
+                  {0:02.02d}.{1:02.02d}.{2:02.02d} {3:02.02d}:{4:02.02d} Uhr
+                </p>
+                <form method="POST" action="/alarm" id="setTime">
+                  <h2 form="setTime">Systemzeit einstellen</h2>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <label for "year">Datum</label>
+                      <input class="form-control" type="text" name="date" id="date" value="{0:02.02d}.{1:02.02d}.{2:02.02d}">
+                    </div>
+                    <div class="col-md-6">
+                      <label for "time">Uhrzeit</label>
+                      <input class="form-control" type="text" name="time" id="time" value="{3:02.02d}:{4:02.02d}">
+                    </div>
+                  </div>
+                  <button class="btn btn-primary" type="submit">absenden</button>
+                </form>
+                <form method="POST" action="/alarm" id="setAlarm">
+                  <h2 form="setAlarm">Weckzeit einstellen</h2>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <label for "alarmTime">Uhrzeit</label>
+                      <input class="form-control" type="text" name="alarmTime" id="alarmTime" value="{5:02.02d}:{6:02.02d}">
+                    </div>
+                    <div class="col-md-6">
+                      <label for "alarmTime">Uhrzeit</label>
+                      <input type="checkbox" name="dailyAlarm" value="dailyAlarm" checked="checked"> daily Alarm
+                      </label>
+                    </div>
+                  </div>
+                  <button class="btn btn-primary" type="submit" name="setAlarm" value="setAlarm">Eingabe absenden</button>
+                </form>
+              </div>
+              <div class="col-md-6">
+                <form method="POST" action="/alarm" id="setSound">
+                  <h2 form="setSound">Wecksound w&auml;hlen</h2>
+                  </br>
+                  <input type="radio" id="MarioMain" name="setSound" value="MarioMain">
+                  <label for="MarioMain"> Super Mario - Main Theme</label>
+                  </br>
+                  <input type="radio" id="MarioTitle" name="setSound" value="MarioTitle">
+                  <label for="MarioTitle"> Super Mario - Title Music</label>
+                  </br>
+                  <input type="radio" id="Looney" name="setSound" value="Looney">
+                  <label for="Looney"> Looney Tunes Theme</label>
+                  </br>
+                  <input type="radio" id="Simpsons" name="setSound" value="Simpsons">
+                  <label for="Simpsons"> The Simpsons</label>
+                  </br>
+                  <input type="radio" id="Indiana" name="setSound" value="Indiana">
+                  <label for="Indiana"> Indiana Jones</label>
+                  </br>
+                  <input type="radio" id="Muppets" name="setSound" value="Muppets">
+                  <label for="Muppets"> The Muppet Show</label>
+                  </br>
+                  <input type="radio" id="Gadget" name="setSound" value="Gadget">
+                  <label for="Gadget"> Inspector Gadget</label>
+                  </br>
+                  <input type="radio" id="StarWars" name="setSound" value="StarWars">
+                  <label for="StarWars"> Star Wars Theme</label>
+                  </br>
+                  <input type="radio" id="Tetris" name="setSound" value="Tetris">
+                  <label for="Tetris"> Tetris Theme</label>
+                  </br>
+                  <button class="btn btn-primary" type="submit" name="setSongButton" value="setSong">Song w&aumlhlen</button>
+                  <!--<button class="btn btn-primary" type="submit" name="playSongButton" value="playSong">Wecksound w&auml;hlen</button>
+                  -->
+                </form>
+              </div>
+            </div>
+        </body>
+        </html>
+    """.format(date[2], date[1], date[0], date[3], date[4], alarmTime[0], alarmTime[1])#.format(0, 1, 2, 3,4, 5)#
     httpResponse.WriteResponseOk(   headers         = ({'Cache-Control': 'no-cache'}),
                                     contentType     = 'text/html',
                                     contentCharset  = 'UTF-8',
