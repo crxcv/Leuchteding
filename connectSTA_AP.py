67eff31f48c5f2beed281afb39d5fd48e1ca3934
Christina Rost <christina.rost@hfg-gmuend.de>
2018-08-21 02:12:09 +0200

    changed conn. alg.

--- connectSTA_AP.py
+++ connectSTA_AP.py
@@ -1,41 +1,46 @@
-import network
+import network, utime
 ap_if = False
 sta_if = False
 
 def connect():
     global ap_if
     global sta_if
-    STA_SSID = "deineSSID"
-    STA_PSK  = "deinPasswort"
+    STA_SSID = "your-ssid"
+    STA_PSK  = "your-PW"
     ssid = "esp32"
     pw = "HuchEinPw"
-    tries = 0
-
-
-    #STA_SSID = home.getSsid()
-    #STA_PSK = home.getPass()
 
     ap_if = network.WLAN(network.AP_IF)
     sta_if = network.WLAN(network.STA_IF)
-    if ap_if.active(): ap_if.active(False)
-    if not ap_if.active():
-        sta_if.active(True)
-        sta_if.connect(STA_SSID, STA_PSK)
-    if not sta_if.isconnected():
-        ap_if.active(True)
-        sta_if.connect(STA_SSID, STA_PSK)
-        print("creating accesspoint")
-        # if not sta_if.isconnected():
-        ap_if.config(essid = ssid)
-        ap_if.config(password = pw)
+
+    sta_if.active(True)
+    sta_if.connect(STA_SSID, STA_PSK)
+
+    timeout = 50
+    while not sta_if.isconnected():
+        utime.sleep_ms(100)
+        timeout -= 1
+        if timeout == 0:
+            break
+    print("======= STA connected ========")
+    print(sta_if.ifconfig())
+
+    print("creating accesspoint")
+    ap_if.active(True)
+    ap_if.config(essid = ssid, password = pw)
+
+    timeout = 50
+    while not ap_if.isconnected(False):
+        utime.sleep_ms(100)
+        timeout -= 1
+        if timeout == 0:
+            break
+    print("======= AP connected ========")
+    print(ap_if.ifconfig)
 
     try:
-        mdns = networkself.mDNS()
+        mdns = network.mDNS()
         mdns.start("RainbowWarrior", "MicroPython with mDNS")
         mdns.addService('_http', '_tcp', 80, "MicroPython", {"board": "ESP32", "service": "mPy Web server"})
     except:
         print("mDNS not started")
-
-def station_activated():
-    print("stationMode activated: {}".format(sta_if))
-    return sta_if
